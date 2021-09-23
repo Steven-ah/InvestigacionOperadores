7a31034d2b79cfdcd32c8a9df57a46239ec59e4d
Steven-ah <78942383+Steven-ah@users.noreply.github.com>
2021-09-22 22:26:33 -0500

    Se finaliza el algoritmo
    Se agrega una tercera bandera y se finaliza el algoritmo

--- SegundoPunto/main.m
+++ SegundoPunto/main.m
@@ -9,9 +9,11 @@ for i=1:nCols
    if i==nCols
         aux2=-1;
         aux3=0;
+        aux4=1;
    else
         aux2=1;
         aux3=1;
+        aux4=0;
    end
    for j=1:nRows
       if j==nRows
@@ -21,8 +23,8 @@ for i=1:nCols
       end 
       resultado(cont,j+(nCols*aux3))=abs(A(j,i) - A(j,i+(1*aux2))); %%% Se puede mantener
       resultado(j+(nCols*aux3),cont)=resultado(cont,j+(nCols*aux3));
-      resultado(j+(1*aux),cont)=abs(A(j,i) - A(j+(1*aux),i)); %%%% Debe de tener bifurcacion
-      resultado(cont,j+(1*aux))=resultado(j+(1*aux),cont);
+      resultado(j+(1*aux)+(nCols*aux4),cont)=abs(A(j,i) - A(j+(1*aux),i)); %%%% Debe de tener bifurcacion
+      resultado(cont,j+(1*aux)+(nCols*aux4))=resultado(j+(1*aux)+(nCols*aux4),cont);
       resultado(cont,j+(nCols*aux3)+(1*aux))=abs(A(j,i) - A(j+(1*aux),i+(1*aux2))); %% debe de tener bifurcacion.
       resultado(j+(nCols*aux3)+(1*aux),cont)=resultado(cont,j+(nCols*aux3)+(1*aux));
       cont=cont+1;
